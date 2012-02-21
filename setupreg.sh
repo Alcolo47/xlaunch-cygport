fbbed7cee822309aa72385fd20337ff2eff5139e
Jon TURNEY <jon.turney@dronecode.org.uk>
2012-02-21 20:33:27 +0000

    Wrap edit/open commands in bash -l
    Wrap edit/open command in bash -l, so xlaunch acquires a login environment
    to pass onto the X server and any local client, if we choose to run the
    configuration.

--- setupreg.sh
+++ setupreg.sh
@@ -2,48 +2,45 @@
 # Add/Remove Windows registry entries to invoke XLaunch from Explorer context menu
 
 # program to invoke
-prog=$(cygpath -w $(which xlaunch))
+prog="\"$(cygpath -w $(which run))\""
+opencmd="\"$(cygpath -w $(which bash))\" -l -c \"xlaunch -run  \\\"%1\\\"\""
+editcmd="\"$(cygpath -w $(which bash))\" -l -c \"xlaunch -load \\\"%1\\\"\""
 
-# HKEY_CURRENT_USER\Software\Classes non-existent on W7
-
-# XXX: either notice if association already exists, and stop
-# XXX: or restore the registry to the previous state
-# (e.g. if there was already an association) by keeping a backup copy
+# ProgID that extension will point to
+ProgID=XLaunch.cygwin
 
 case "$1" in
-remove)
-	regtool -v remove "/HKEY_CLASSES_ROOT/.xlaunch"
+    remove)
+        # MSFT guidelines say only remove the ProgID, not the extension
+	# when uninstalled, in case it points to another application now
 
-	regtool -v remove "/HKEY_CLASSES_ROOT/XLaunch/Shell/Open/command"
-	regtool -v remove "/HKEY_CLASSES_ROOT/XLaunch/Shell/Open"
+	regtool -v remove "/HKEY_CLASSES_ROOT/$ProgID/Shell/Open/command"
+	regtool -v remove "/HKEY_CLASSES_ROOT/$ProgID/Shell/Open"
 
-	regtool -v remove "/HKEY_CLASSES_ROOT/XLaunch/Shell/Edit/command"
-	regtool -v remove "/HKEY_CLASSES_ROOT/XLaunch/Shell/Edit"
+	regtool -v remove "/HKEY_CLASSES_ROOT/$ProgID/Shell/Edit/command"
+	regtool -v remove "/HKEY_CLASSES_ROOT/$ProgID/Shell/Edit"
 
-	regtool -v remove "/HKEY_CLASSES_ROOT/XLaunch/Shell"
+	regtool -v remove "/HKEY_CLASSES_ROOT/$ProgID/Shell"
 
-	regtool -v remove "/HKEY_CLASSES_ROOT/XLaunch"
+	regtool -v remove "/HKEY_CLASSES_ROOT/$ProgID"
 	;;
 
-add)
+    add)
 	regtool -s -v add "/HKEY_CLASSES_ROOT/.xlaunch"
-	regtool -s -v set "/HKEY_CLASSES_ROOT/.xlaunch/" "XLaunch"
+	regtool -s -v set "/HKEY_CLASSES_ROOT/.xlaunch/" "$ProgID"
 
-	regtool -s -v add "/HKEY_CLASSES_ROOT/XLaunch"
-	regtool -s -v add "/HKEY_CLASSES_ROOT/XLaunch/Shell"
+	regtool -s -v add "/HKEY_CLASSES_ROOT/$ProgID"
+	regtool -s -v add "/HKEY_CLASSES_ROOT/$ProgID/Shell"
 
-	regtool -s -v add "/HKEY_CLASSES_ROOT/XLaunch/Shell/Open"
-	regtool -s -v add "/HKEY_CLASSES_ROOT/XLaunch/Shell/Open/command"
-	regtool -s -v set "/HKEY_CLASSES_ROOT/XLaunch/Shell/Open/command/" "\"$prog\" -run \"%1\""
+	regtool -s -v add "/HKEY_CLASSES_ROOT/$ProgID/Shell/Open"
+	regtool -s -v add "/HKEY_CLASSES_ROOT/$ProgID/Shell/Open/command"
+	regtool -s -v set "/HKEY_CLASSES_ROOT/$ProgID/Shell/Open/command/" "$prog $opencmd"
 
-	regtool -s -v add "/HKEY_CLASSES_ROOT/XLaunch/Shell/Edit"
-	regtool -s -v add "/HKEY_CLASSES_ROOT/XLaunch/Shell/Edit/command"
-	regtool -s -v set "/HKEY_CLASSES_ROOT/XLaunch/Shell/Edit/command/" "\"$prog\" -load \"%1\""
-
+	regtool -s -v add "/HKEY_CLASSES_ROOT/$ProgID/Shell/Edit"
+	regtool -s -v add "/HKEY_CLASSES_ROOT/$ProgID/Shell/Edit/command"
+	regtool -s -v set "/HKEY_CLASSES_ROOT/$ProgID/Shell/Edit/command/" "$prog $editcmd"
 	;;
 esac
 
 # never report errors
 true
-
-# End
