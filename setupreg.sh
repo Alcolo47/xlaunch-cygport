2b067de132914a19d35fe9be02e4907965edca43
Jon TURNEY <jon.turney@dronecode.org.uk>
2012-03-11 14:18:39 +0000

    Register xlaunch document icon

--- setupreg.sh
+++ setupreg.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 # Add/Remove Windows registry entries to invoke XLaunch from Explorer context menu
 
+# XXX: /HKCR is a merged view of /HKLM/Software/Classes and /HKCU/Software/Classes
+# Does writing work correctly for Admin and non-admin users?
+
 # program to invoke
 prog="\"$(cygpath -w $(which run))\""
 opencmd="\"$(cygpath -w $(which bash))\" -l -c \"xlaunch -run  \\\"%1\\\"\""
 editcmd="\"$(cygpath -w $(which bash))\" -l -c \"xlaunch -load \\\"%1\\\"\""
+
+# document icon to use
+# (we must set this or we get the run icon, which isn't what we want)
+icon="$(cygpath -w $(which xlaunch)).exe,1"
 
 # ProgID that extension will point to
 ProgID=XLaunch.cygwin
@@ -22,12 +29,15 @@ case "$1" in
 
 	regtool -v remove "/HKEY_CLASSES_ROOT/$ProgID/Shell"
 
+	regtool -v remove "/HKEY_CLASSES_ROOT/$ProgID/DefaultIcon"
+
 	regtool -v remove "/HKEY_CLASSES_ROOT/$ProgID"
 	;;
 
     add)
 	regtool -s -v add "/HKEY_CLASSES_ROOT/.xlaunch"
 	regtool -s -v set "/HKEY_CLASSES_ROOT/.xlaunch/" "$ProgID"
+	# XXX: should also add to OpenWithProgIds
 
 	regtool -s -v add "/HKEY_CLASSES_ROOT/$ProgID"
 	regtool -s -v add "/HKEY_CLASSES_ROOT/$ProgID/Shell"
@@ -39,6 +49,9 @@ case "$1" in
 	regtool -s -v add "/HKEY_CLASSES_ROOT/$ProgID/Shell/Edit"
 	regtool -s -v add "/HKEY_CLASSES_ROOT/$ProgID/Shell/Edit/command"
 	regtool -s -v set "/HKEY_CLASSES_ROOT/$ProgID/Shell/Edit/command/" "$prog $editcmd"
+
+	regtool -s -v add "/HKEY_CLASSES_ROOT/$ProgID/DefaultIcon"
+	regtool -s -v set "/HKEY_CLASSES_ROOT/$ProgID/DefaultIcon/" "$icon"
 	;;
 esac
 
